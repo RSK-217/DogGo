a160873b7df7e326e530b9291f2e5430f6d5d4a9
Rick Kersmarki <rkersmarki@gmail.com>
2022-09-17 12:59:48 -0500

    Add project files.

---
+++ DogGo/Data/DogGoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DogGo.Models;
+
+namespace DogGo.Data
+{
+    public class DogGoContext : DbContext
+    {
+        public DogGoContext (DbContextOptions<DogGoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DogGo.Models.Walker> Walker { get; set; } = default!;
+    }
+}
